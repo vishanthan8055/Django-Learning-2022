04eed0f933ae3b32213fb1b61fdbb30dc2288619
Vishanthan <vishanthan8055@gmail.com>
2022-02-01 20:05:38 +0530

    if loops

--- Javascript/Basics/app2.js
+++ Javascript/Basics/app2.js
@@ -1,4 +1,46 @@
-2 == 2;
-3 == 3;
-console.log(3+4);
-console.log(3+4,'hello',true);
+// if(1 !== 1){
+//     console.log("Its true");
+// }
+//Example 2
+
+// let rating = 2;
+// if(rating<3){
+//     console.log("You are not a superstar")
+// }
+
+// // example 3
+// let num = 37;
+
+// if(num%2 == 0){
+//     console.log("Its even number");
+// }
+// else{
+//     console.log("Odd number bro!!");
+// }
+// example else if
+let rating = 'a';
+if (rating === 3){
+    console.log("You are a superstar");
+}
+else if (rating === 2) {
+    console.log("meet expectations!");
+}
+else if (rating === 1) {
+    console.log("needs improvement");
+}
+else {
+    console.error("Invalid rating!!");
+    // console.log("Invalid rating!!");
+}
+//example 
+let highScore = 1400;
+let userScore = 10300;
+
+if (userScore >= highScore) {
+    console.log(`Congrats, you have the new 
+    high score of ${userScore}`);
+    highScore = userScore;
+}
+else {
+    console.log(`Good game!..your score is ${userScore}..But the highscore is ${highScore}`);
+}

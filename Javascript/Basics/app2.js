7f5bc08adc82b7544567dce5c88e88c6d8f91ade
Vishanthan <vishanthan8055@gmail.com>
2022-02-03 19:25:23 +0530

    ..

--- Javascript/Basics/app2.js
+++ Javascript/Basics/app2.js
@@ -18,29 +18,31 @@
 //     console.log("Odd number bro!!");
 // }
 // example else if
-let rating = 'a';
-if (rating === 3){
-    console.log("You are a superstar");
-}
-else if (rating === 2) {
-    console.log("meet expectations!");
-}
-else if (rating === 1) {
-    console.log("needs improvement");
-}
-else {
-    console.error("Invalid rating!!");
-    // console.log("Invalid rating!!");
-}
-//example 
-let highScore = 1400;
-let userScore = 10300;
+// let rating = 'a';
+// if (rating === 3){
+//     console.log("You are a superstar");
+// }
+// else if (rating === 2) {
+//     console.log("meet expectations!");
+// }
+// else if (rating === 1) {
+//     console.log("needs improvement");
+// }
+// else {
+//     console.error("Invalid rating!!");
+//     // console.log("Invalid rating!!");
+// }
+// //example 
+// let highScore = 1400;
+// let userScore = 10300;
 
-if (userScore >= highScore) {
-    console.log(`Congrats, you have the new 
-    high score of ${userScore}`);
-    highScore = userScore;
-}
-else {
-    console.log(`Good game!..your score is ${userScore}..But the highscore is ${highScore}`);
-}
+// if (userScore >= highScore) {
+//     console.log(`Congrats, you have the new 
+//     high score of ${userScore}`);
+//     highScore = userScore;
+// }
+// else {
+//     console.log(`Good game!..your score is ${userScore}..But the highscore is ${highScore}`);
+// }
+
+
